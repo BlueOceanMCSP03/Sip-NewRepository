8432952f008862b912c3aa39b86c1bf2c8df8f4f
GJoker94 <ellisjr.griffin@gmail.com>
2020-03-11 15:21:30 -0500

    updated Databases part 2

--- database/schema.sql
+++ database/schema.sql
@@ -3,9 +3,31 @@ CREATE DATABASE Sip;
 
 USE Sip;
 
-CREATE TABLE profiles (
-    Id INT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    drink VARCHAR(255) NOT NULL
+CREATE TABLE Profiles (
+    Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL,
+    Location VARCHAR(255) NOT NULL,
+    Userpassword VARCHAR(255) NOT NULL,
+    Profileimages VARCHAR (255) NOT NULL
 );
+
+CREATE TABLE Reviews(
+    Id INT NOT NULL PRIMARY KEY,
+    Namedrink VARCHAR(255) NOT NULL,
+    Store VARCHAR(255) NOT NULL,
+    Rating INT (255) NOT NULL,
+    Comments VARCHAR(255) NOT NULL,
+    Time VARCHAR(255)NOT NULL,
+    Drinkimage VARCHAR(255) NOT NULL
+    
+);
+
+CREATE TABLE Drinks(
+    Id INT NOT NULL PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL,
+    Drinkname VARCHAR (255) NOT NULL,
+    DrinkImage VARCHAR(255) NOT NULL,
+    Favoritedrink BOOLEAN NOT NULL
+
+
+)
