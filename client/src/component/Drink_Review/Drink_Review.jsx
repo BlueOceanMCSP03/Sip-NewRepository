dd9f9203201d7f9613cf65868f14c8658be54eef
Emilio-miloCastaneda <57383306+Emilio-miloCastaneda@users.noreply.github.com>
2020-03-12 17:09:32 -0500

    reconfigured the drink Review.jsx file to be classical

--- client/src/component/Drink_Review/Drink_Review.jsx
+++ client/src/component/Drink_Review/Drink_Review.jsx
@@ -1,42 +1,67 @@
-import React from 'react'; 
+import React, { Component } from 'React'; 
 import style from './Drink_Review.test';
+import StarRatingComponent from 'react-star-rating-component';
 
-const Drink_Review = () => {
-
-
-    return ( 
+export default class Drink_Review extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      location: '',
+      drinkName: ''
+    }
+    this.echandleLocation = this.echandleLocation.bind(this)
     
-    <div>
-      <div className={style.userReview}>
-        <div className={style.location}>
-          <h1 className={style.text}>Location</h1>
-          <input></input>
-        </div>
-      
-        <div className='drink'>
-          <h1 className={style.text}>Drink</h1>
-          <input></input>
-        </div>
+  }
+      echandleLocation(event) {
+        this.setState({
+          location: event.target.value   
+        })
+        console.log(this.state.location)
+      }
 
-        <div className='uploadImage'>
-          <div className='uploadimagebar'>
-            <h1 className={style.text}>uploadImage</h1>
-            <input type='text'></input><button>Add Image</button>
+      echandleDrink(event) {
+        this.setState({
+          drinkName: event.target.value
+        })
+        console.log()
+      }
+
+    render() {
+      
+      return ( 
+    
+      <div>
+        <div className={style.userReview}>
+          <div className={style.location}>
+            <h1 className={style.text}>Location</h1>
+            <input type='text' maxLength='255' onChange={this.echandleLocation}></input>
+          </div>
+
+          <div className='drink'>
+            <h1 className={style.text}>Drink</h1>
+            <input></input>
+          </div>
+
+          <div className='uploadImage'>
+            <div className='uploadimagebar'>
+              <h1 className={style.text}>uploadImage</h1>
+              <input type='text'></input><input type='submit'  Value='Submit' required></input>
+            </div>
+          </div>
+
+          <div className='rating/comment'>
+            <h1 className={style.text}>Rating:</h1>
+              <StarRatingComponent />
+            <h1 className={style.text}>Comment:</h1>
+              <input type="text"></input>
+              <br></br>
+              <input type='checkbox' required></input><h1 className={style.text}>Mark as favorite drink</h1>
+
+              <button onClick="">Submit</button>
           </div>
         </div>
-
-        <div className='rating/comment'>
-          <h1 className={style.text}>Rating:</h1>
-        {/* <StarRatingComponent /> */}
-          <h1 className={style.text}>Comment:</h1>
-          <input type="text"></input>
-          <br></br>
-          <button></button><h1 className={style.text}>Mark as favorite drink</h1>
-          <button>Submit</button>
-        </div>
       </div>
-    </div>
-    )
-};
 
-export default Drink_Review;
+      )
+    }
+};
