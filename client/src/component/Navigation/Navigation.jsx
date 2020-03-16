757c9aae60b0122767d17eb7e35240ac84e1d74c
Martin Ramos <57451583+NitramSomar@users.noreply.github.com>
2020-03-16 14:20:50 -0500

    updated buttons

--- client/src/component/Navigation/Navigation.jsx
+++ client/src/component/Navigation/Navigation.jsx
@@ -3,10 +3,9 @@ import styleme from './Navigation.module.css';
 const Navigation = () => {
     return (
         <div>
-            <button className={styleme.button}>Review</button>
-
-            <button className={styleme.button}>Profile</button>
-            <button className={styleme.button}>Nearby</button>
+            <button className={styleme.button1}><i className="glyphicon glyphicon-search"></i></button>
+            <button className={styleme.button2}><i className={`${styleme.icon}`}></i></button>
+            <button className={`${styleme.button3} ${styleme.tablink}`}>Review</button>
         </div>
     )
 }
