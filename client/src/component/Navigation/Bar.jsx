c0e329fbb0209ac030b9345929661736adf6e39b
Martin Ramos <57451583+NitramSomar@users.noreply.github.com>
2020-03-16 18:26:13 -0500

    updated typo

--- client/src/component/Navigation/Bar.jsx
+++ client/src/component/Navigation/Bar.jsx
@@ -7,7 +7,7 @@ import history from '../../Routing/history';
 const Bar = () => {
     return (
         <div>
-            <button className={styleme.button1} onClick={() => history.push('/Review')}><i className="fa fa-star" style={{'fontSize':'2.5rem'}} value="5"></i> </button>
+            <button className={styleme.button1} onClick={() => history.push('/Review')}><i className="fa fa-star" style={{'fontSize':'2.5rem'}} value="5"></i></button>
             <button className={`${styleme.button2}`} onClick={() => history.push('/Profile')}><i className="menu_icon fa fa-child" style={{'fontSize':'4rem'}}></i></button>
             <button className={`${styleme.button3} ${styleme.tablink}`} onClick={() => history.push('/Nearby')}><i className="fa fa-bitbucket" style={{'fontSize':'2.5rem'}}></i></button>
         </div>
