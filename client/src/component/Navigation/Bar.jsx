35d004b5fdf724bf24f7553a3871ae6133a616d6
Martin Ramos <57451583+NitramSomar@users.noreply.github.com>
2020-03-16 17:59:12 -0500

    finalized buttons

--- client/src/component/Navigation/Bar.jsx
+++ client/src/component/Navigation/Bar.jsx
@@ -7,10 +7,9 @@ import history from '../../Routing/history';
 const Bar = () => {
     return (
         <div>
-            <button className={styleme.button1} onClick={() => history.push('/Review')}>Review</button>
-            <button className={`${styleme.button2}`} onClick={() => history.push('/Profile')}>Profile</button>
-            <button className={`${styleme.button3} ${styleme.tablink}`} onClick={() => history.push('/Nearby')}>Nearby</button>
-            <button className={`${styleme.button4}`}>logout</button>
+            <button className={styleme.button1} onClick={() => history.push('/Review')}><i className="fa fa-star" style={{'fontSize':'2.5rem'}} value="5"></i> </button>
+            <button className={`${styleme.button2}`} onClick={() => history.push('/Profile')}><i className="menu_icon fa fa-child" style={{'fontSize':'4rem'}}></i></button>
+            <button className={`${styleme.button3} ${styleme.tablink}`} onClick={() => history.push('/Nearby')}><i className="fa fa-bitbucket" style={{'fontSize':'2.5rem'}}></i></button>
         </div>
     )
 }
