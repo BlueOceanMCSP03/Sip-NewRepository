94f76c9ec108dc5c2ac37f062886052506eb91d2
Poli <57406032+ovopoli@users.noreply.github.com>
2020-03-18 12:47:58 -0500

    working index.html

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
-import Shop_Profile from './component/Shop_Profile/Shop_Profile'
+
 
 
 ReactDOM.render(
-    <Shop_Profile />,
+    <App />,
     document.getElementById('app')
 );
